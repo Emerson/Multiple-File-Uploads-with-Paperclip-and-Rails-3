3d7efe8b4c2206d0a27cb6089e5d5b99cda879ab
Emerson Lackey <emerson.lackey@gmail.com>
2011-02-05 15:15:43 -0500

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipRails3::Application.config.secret_token = 'a3480ec2d188bf0ba889394f1d5cc0a55c4d77523989a644793a1abf6d085062bb74d86243a920a0e9fc8498f49229aefc72695b81caa3573b49328508ef216b'
